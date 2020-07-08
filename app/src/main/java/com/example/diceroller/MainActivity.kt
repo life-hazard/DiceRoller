de05d1fd51f25d8e72da04778a797faf2f36a3ae
life_hazard <gabrielamirek98@gmail.com>
2020-07-08 23:02:04 +0200

    Added visual representation of dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlin.random.Random
+
+//private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +22,28 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(viewRoot)
 
-        //binding.rollButton.text = "Let\'s roll"
+        binding.rollButton.text = "Let\'s roll"
+
+        binding.rollButton.setOnClickListener {
+            Toast.makeText(this, "rolled", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    // let's see if it works to bind things in methods
+
+
+    private fun rollDice() {
+
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)   //can't put it into binding - the app crashes
     }
 }
